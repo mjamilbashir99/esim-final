e0e7d5bf8a3e66595b250be46ef8117459949bb1
Ismail <ismail1@gmail.com>
2025-07-18 11:55:42 +0500

    Update header

--- core/app/Views/layouts/default.php
+++ core/app/Views/layouts/default.php
@@ -77,12 +77,12 @@
   </div>
 
   <!-- Bottom Wave Divider -->
-  <!-- <div style="position:absolute; bottom:-1px; width:100%; overflow:hidden; line-height:0;">
+  <div style="position:absolute; bottom:-1px; width:100%; overflow:hidden; line-height:0;">
     <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 60px; width: 100%;">
       <path d="M0.00,49.98 C150.00,150.00 349.33,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
         style="stroke: none; fill: #ffffff;"></path>
     </svg>
-  </div> -->
+  </div>
 </section>
 
 
